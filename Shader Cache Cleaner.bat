25eabc1bf3ffb44e49473aa67c9d9f32e16c3f51
Gametism <157929762+Gametism@users.noreply.github.com>
2024-10-10 22:38:46 +0200

    Update Shader Cache Cleaner.bat

--- Shader Cache Cleaner.bat
+++ Shader Cache Cleaner.bat
@@ -20,13 +20,13 @@ set "Intel_cache2=%LOCALAPPDATA%\Low\Intel\ShaderCache"
 :: Create log file
 set "LogFile=%LOCALAPPDATA%\ShaderCacheCleaner.log"
 echo Shader Cache Cleaner Log > "%LogFile%"
-echo Version 0.1 >> "%LogFile%"
+echo Version 0.2 >> "%LogFile%"
 echo --------------------------------------- >> "%LogFile%"
 echo Started at: %date% %time% >> "%LogFile%"
 echo. >> "%LogFile%"
 
 echo Welcome to the Shader Cache Cleaner by Gametism!
-echo Version 0.1
+echo Version 0.2
 echo.
 
 :: Cache cleaning process starts here
@@ -42,28 +42,27 @@ echo %Intel_cache2%
 :: Confirmation for deletion
 choice /c YN /M "Are you sure you want to delete these directories?"
 if %ERRORLEVEL% == 2 (
-    echo Deletion canceled.
-    goto end
+    echo Deletion of graphics driver caches canceled.
+    echo Graphics driver cache deletion skipped. >> "%LogFile%"
+) else (
+    echo Starting cache cleaning process...
+    echo. >> "%LogFile%"
+    :: Delete graphics driver shader caches
+    call :delete_cache "%NVidia_cache1%" "NVIDIA DXCache"
+    call :delete_cache "%NVidia_cache2%" "NVIDIA GLCache"
+    call :delete_cache "%NVidia_cache3%" "NVIDIA NV_Cache"
+    call :delete_cache "%AMD_cache1%" "AMD DXCache"
+    call :delete_cache "%AMD_cache2%" "AMD GLCache"
+    call :delete_cache "%Intel_cache1%" "Intel ShaderCache"
+    call :delete_cache "%Intel_cache2%" "Intel Low ShaderCache"
 )
 
-:: Ask if user wants to add a custom path to delete
+:: Ask if user wants to add a custom path to delete, even if they chose not to delete the shader caches
 set "CustomGameCache="
 choice /c YN /M "Do you want to add a custom path for a game shader cache?"
 if %ERRORLEVEL% == 1 (
     set /p CustomGameCache="Please copy and paste the path to the shader cache folder or file you want to delete: "
 )
-
-echo Starting cache cleaning process...
-echo. >> "%LogFile%"
-
-:: Function to delete cache if it exists
-call :delete_cache "%NVidia_cache1%" "NVIDIA DXCache"
-call :delete_cache "%NVidia_cache2%" "NVIDIA GLCache"
-call :delete_cache "%NVidia_cache3%" "NVIDIA NV_Cache"
-call :delete_cache "%AMD_cache1%" "AMD DXCache"
-call :delete_cache "%AMD_cache2%" "AMD GLCache"
-call :delete_cache "%Intel_cache1%" "Intel ShaderCache"
-call :delete_cache "%Intel_cache2%" "Intel Low ShaderCache"
 
 :: Check if user provided a custom path and delete it
 if defined CustomGameCache (
